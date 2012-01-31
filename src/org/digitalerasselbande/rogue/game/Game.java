3c2e188b3ad724864a5a2d357240da769dcc9fa0
georg wiltschek <georg.wiltschek@gmail.com>
2012-01-31 14:50:46 +0100

    removed unused imports

--- src/org/digitalerasselbande/rogue/game/Game.java
+++ src/org/digitalerasselbande/rogue/game/Game.java
@@ -1,10 +1,8 @@
 package org.digitalerasselbande.rogue.game;
 
 import java.io.BufferedReader;
-import java.io.Console;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.Reader;
 
 import org.digitalerasselbande.rogue.entity.Monster;
 import org.digitalerasselbande.rogue.entity.Player;
