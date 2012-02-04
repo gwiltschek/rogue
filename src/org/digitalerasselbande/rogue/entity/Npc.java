18dc43682db5ab3f69f34c418dfdc6f89a49474d
georg wiltschek <georg.wiltschek@gmail.com>
2012-02-04 12:02:51 +0100

    more stub methods for quests/npc

--- src/org/digitalerasselbande/rogue/entity/Npc.java
+++ src/org/digitalerasselbande/rogue/entity/Npc.java
@@ -1,20 +1,51 @@
 package org.digitalerasselbande.rogue.entity;
 
+import java.util.LinkedList;
 import java.util.Random;
 
 import org.digitalerasselbande.rogue.map.Map;
+import org.digitalerasselbande.rogue.quest.Quest;
 
 public class Npc extends Entity {
 
 	private Map map;
+	private LinkedList<Quest> quests;
 	
 	public Npc(Map map) {
-		this.setHealth(9999999); // really hard to kill
+		this.setHealth(Integer.MAX_VALUE); // really hard to kill
 		this.setSymbol("N");
 		this.setSymbolString(this.getSymbol());
 		this.setIsPushable(false);
 		this.setType(EntityType.NPC);
 		this.map = map;
+		generateQuests();
 	}
 
+	private void generateQuests() {
+		
+	}
+	
+	public LinkedList<Quest> getCurrentQuests() {
+		LinkedList<Quest> cq = null;
+		
+		for (Quest q : quests) {
+			if (q.isActive() && !q.isFinished()) {
+				cq.add(q);				
+			}
+		}
+		return cq;
+	}
+	
+	public Quest getNewQuest() {
+		if (quests.size() > 0) {
+			return quests.getFirst();
+		} else {
+			generateQuests();
+			return getNewQuest();
+		}
+	}
+	
+	public boolean checkQuestFinished(Quest q) {
+		return q.isFinished();
+	}
 }
--- src/org/digitalerasselbande/rogue/quest/Quest.java
+++ src/org/digitalerasselbande/rogue/quest/Quest.java
@@ -3,16 +3,33 @@ package org.digitalerasselbande.rogue.quest;
 import java.util.LinkedList;
 
 import org.digitalerasselbande.rogue.entity.Entity;
+import org.digitalerasselbande.rogue.entity.Player;
 import org.digitalerasselbande.rogue.item.Item;
 
 public class Quest {
 
 	private Entity employer;
+	private Player player;
 	private QuestType type;
+	private boolean active = false;
 	private boolean finished = false;
 	private int rewardXP;
 	private int rewardGold;
 	private LinkedList<Item> rewardItems;
 
 	private String description;
+	
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public boolean isFinished() {
+		return finished;
+	}
+
+	
 }
